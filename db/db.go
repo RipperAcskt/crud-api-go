df378436f31113b25d6702d055d1d305fbf4638e
Ripper <7309333@gmail.com>
2022-10-08 19:39:17 +0300

    add package json which working with json information and keep structers person and deleteInformation. Also added db package which working database and keep endoint for http

---
+++ db/db.go
@@ -0,0 +1,222 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/RipperAcskt/crud-api-go/json"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type Database struct {
+	DbObject *sql.DB
+}
+
+func (db Database) List(w http.ResponseWriter, req *http.Request) {
+	var id, age int
+	var firstName, lastName string
+
+	rows, err := db.DbObject.Query("SELECT * FROM Person ORDER BY id")
+
+	if err != nil {
+		log.Fatalf("Error while doing request to database for output table: %v\n", err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		if err = rows.Scan(&id, &firstName, &lastName, &age); err != nil {
+			fmt.Fprintf(w, "%v\n", err)
+			continue
+		}
+		fmt.Fprintf(w, "Id: %d\nName: %s\nSurname: %s\nAge: %d\n", id, firstName, lastName, age)
+	}
+}
+
+func (db Database) Create(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(w, "Bad method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var personToCreate json.Person
+
+	errJson := json.JsonUnmarshal(req.Body, &personToCreate, false)
+
+	if errJson != "" {
+		http.Error(w, errJson, 500)
+		return
+	}
+
+	var badRequest string
+
+	if personToCreate.Name == "" {
+		badRequest += "Fill name\n"
+	}
+	if personToCreate.Surname == "" {
+		badRequest += "Fill surname\n"
+	}
+	if personToCreate.Age <= 0 {
+		badRequest += "Age need to be upper zero\n"
+	}
+	if badRequest != "" {
+		http.Error(w, badRequest, http.StatusBadRequest)
+		return
+	}
+
+	_, err := db.DbObject.Exec("INSERT INTO person(firstName, lastName, age) VALUES($1, $2, $3)", personToCreate.Name, personToCreate.Surname, personToCreate.Age)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while creating person: %v", err), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.JsonMarshalResponse("Created")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while marshalling: %v\n", err), 500)
+		return
+	}
+	w.Write(response)
+
+}
+
+func (db Database) Delete(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodDelete {
+		http.Error(w, "Bad method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var deleteRequest json.DeleteInfo
+
+	errJson := json.JsonUnmarshal(req.Body, &deleteRequest, true)
+
+	if errJson != "" {
+		http.Error(w, errJson, 500)
+		return
+	}
+
+	if deleteRequest.Id <= 0 && !deleteRequest.DeleteAllTable {
+		http.Error(w, "Id should be upper than zero", http.StatusBadRequest)
+		return
+	}
+
+	stmt, err := db.DbObject.Prepare("DELETE FROM Person WHERE id = $1")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while preparring request to database: %v", err), 500)
+		return
+	}
+	defer stmt.Close()
+
+	if deleteRequest.DeleteAllTable {
+		_, err := db.DbObject.Exec("DELETE FROM Person")
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error while deleting all information from table: %v\n", err), 500)
+			return
+		}
+	}
+	_, err = stmt.Exec(deleteRequest.Id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while deleting id-%d: %v\n", deleteRequest.Id, err), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.JsonMarshalResponse("Deleted")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while marshalling: %v\n", err), 500)
+		return
+	}
+	w.Write(response)
+}
+
+func (db Database) UpdateAll(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPut {
+		http.Error(w, "Bad method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var personToUpdate json.Person
+
+	errJson := json.JsonUnmarshal(req.Body, &personToUpdate, false)
+
+	if errJson != "" {
+		http.Error(w, errJson, 500)
+		return
+	}
+
+	if personToUpdate.Name == "" || personToUpdate.Surname == "" {
+		http.Error(w, "All fields should be filled in", http.StatusBadRequest)
+		return
+	}
+
+	_, err := db.DbObject.Exec("UPDATE Person SET firstName = $1, lastName = $2, age = $3 WHERE id = $4", personToUpdate.Name, personToUpdate.Surname, personToUpdate.Age, personToUpdate.Id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while updating: %v\n", err), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.JsonMarshalResponse("Updated all")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while marshalling: %v\n", err), 500)
+		return
+	}
+	w.Write(response)
+
+}
+
+func (db Database) Update(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPatch {
+		http.Error(w, "Bad method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	personToUpdate := json.Person{Age: -1}
+
+	errJson := json.JsonUnmarshal(req.Body, &personToUpdate, false)
+
+	if errJson != "" {
+		http.Error(w, errJson, 500)
+		return
+	}
+
+	var err error
+
+	if personToUpdate.Name != "" && personToUpdate.Surname != "" && personToUpdate.Age != -1 {
+		http.Error(w, "For updating all field go to /updateAll", http.StatusBadRequest)
+		return
+	}
+
+	if personToUpdate.Name != "" {
+		_, err = db.DbObject.Exec("UPDATE Person SET firstName = $1 WHERE id = $2", personToUpdate.Name, personToUpdate.Id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error while updating: %v\n", err), 500)
+			return
+		}
+	}
+	if personToUpdate.Surname != "" {
+		_, err = db.DbObject.Exec("UPDATE Person SET lastName = $1 WHERE id = $2", personToUpdate.Surname, personToUpdate.Id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error while updating: %v\n", err), 500)
+			return
+		}
+	}
+	if personToUpdate.Age != -1 {
+		_, err = db.DbObject.Exec("UPDATE Person SET age = $1 WHERE id = $2", personToUpdate.Age, personToUpdate.Id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error while updating: %v\n", err), 500)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.JsonMarshalResponse("Updated")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while marshalling: %v\n", err), 500)
+		return
+	}
+	w.Write(response)
+}
---
+++ json/json.go
@@ -0,0 +1,48 @@
+package json
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+type Person struct {
+	Id      int
+	Name    string `json:"name"`
+	Surname string `json:"lastName"`
+	Age     int    `json:"age"`
+}
+
+type DeleteInfo struct {
+	Id             int
+	DeleteAllTable bool `json:"all"`
+}
+
+func JsonUnmarshal(body io.ReadCloser, s interface{}, delete bool) string {
+	readedBody, err := ioutil.ReadAll(body)
+	if err != nil {
+		return fmt.Sprintf("Error while reading body: %v", err)
+	}
+	if delete {
+		err = json.Unmarshal(readedBody, s.(*DeleteInfo))
+	} else {
+		err = json.Unmarshal(readedBody, s.(*Person))
+	}
+
+	if err != nil {
+		return fmt.Sprintf("Error while unmarshal: %v", err)
+	}
+
+	return ""
+}
+
+func JsonMarshalResponse(message string) ([]byte, error) {
+	resp := make(map[string]string)
+	resp["Status"] = message
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		return nil, err
+	}
+	return jsonResp, nil
+}
