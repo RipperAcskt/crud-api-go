6fa3ad6721f208ec9145515e18e1ffd76c0f469a
Ripper <7309333@gmail.com>
2022-10-08 17:43:52 +0300

    add updateAll endpoit which updating all information about person

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type database struct {
 }
 
 type person struct {
+	Id      int
 	Name    string `json:"name"`
 	Surname string `json:"lastName"`
 	Age     int    `json:"age"`
@@ -145,6 +146,49 @@ func (db database) delete(w http.ResponseWriter, req *http.Request) {
 	w.Write(jsonResp)
 }
 
+func (db database) updateAll(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPut {
+		http.Error(w, "Bad method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while reading body: %v", err), 500)
+		return
+	}
+
+	var personToUpdate person
+
+	err = json.Unmarshal(body, &personToUpdate)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while unmarshal: %v", err), 500)
+		return
+	}
+
+	if personToUpdate.Name == "" || personToUpdate.Surname == "" {
+		http.Error(w, "All fields should be filled in", http.StatusBadRequest)
+		return
+	}
+
+	_, err = db.dbObject.Exec("UPDATE Person SET firstName = $1, lastName = $2, age = $3 WHERE id = $4", personToUpdate.Name, personToUpdate.Surname, personToUpdate.Age, personToUpdate.Id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while updating: %v\n", err), 500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["Status"] = "Updated"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error while marshalling: %v\n", err), 500)
+		return
+	}
+	w.Write(jsonResp)
+
+}
+
 func main() {
 	url := "postgres://ripper:150403@localhost:5432/ripper"
 
@@ -156,6 +200,7 @@ func main() {
 	mux.Handle("/list", http.HandlerFunc(db.list))
 	mux.Handle("/create", http.HandlerFunc(db.create))
 	mux.Handle("/delete", http.HandlerFunc(db.delete))
+	mux.Handle("/updateAll", http.HandlerFunc(db.updateAll))
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 
 }
@@ -166,39 +211,4 @@ func openDB(url string) *sql.DB {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	return db
-}
-
-func update(db *sql.DB) {
-	var id, age, n int
-	var firstName, lastName string
-
-	fmt.Print("Id: ")
-	fmt.Scanln(&id)
-	fmt.Printf("1-Имя\n2-Фамилия\n3-Возвраст\n")
-	fmt.Scanln(&n)
-
-	switch n {
-	case 1:
-		fmt.Print("Имя: ")
-		fmt.Scanln(&firstName)
-		_, err := db.Exec("UPDATE Person SET firstName = $1 WHERE id = $2", firstName, id)
-		if err != nil {
-			log.Fatalf("Error while updating firstName: %v\n", err)
-		}
-	case 2:
-		fmt.Print("Фамилия: ")
-		fmt.Scanln(&lastName)
-		_, err := db.Exec("UPDATE Person SET lastName = $1 WHERE id = $2", lastName, id)
-		if err != nil {
-			log.Fatalf("Error while updating lastName: %v\n", err)
-		}
-	case 3:
-		fmt.Print("Возраст: ")
-		fmt.Scanln(&age)
-		_, err := db.Exec("UPDATE Person SET age = $1 WHERE id = $2", age, id)
-		if err != nil {
-			log.Fatalf("Error while updating age: %v\n", err)
-		}
-
-	}
 }
